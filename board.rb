e667621fef8c3dc28924c88fa6370c9d8c3e43c4
James Bergin <javascriptjames23@gmail.com>
2017-07-11 13:32:18 +0100

    board display update and check for a win working

--- board.rb
+++ board.rb
@@ -1,7 +1,8 @@
 class Board
 	attr_accessor 
 	def initialize
-		@board = [1,2,3,4,5,6,7,8,9]
+		@board = ["1","2","3","4","X","6","7","8","9"]
+		@wins_combo = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]]
 	end
 	def draw_board
 		puts "Tic-Tac-To Board \n \n"
@@ -19,10 +20,34 @@ class Board
 	def reset_board
 		@board = @board = [1,2,3,4,5,6,7,8,9]
 	end
+
+	def check_win
+		results =[]
+		@wins_combo.each do |set|
+		  set.map do |i|
+			   if @board[i] == "X"
+			 	  results.push(@board[i])
+			   end
+		  end
+		    check_results(results)
+		    results = []
+		end
+	end
+	def check_results(results)
+		 if results[0..2] == ["X","X","X"]
+		 	  p "X WINS!!!!! "
+		 end
+	end
 end
 a=Board.new
 a.draw_board
-a.update_board(4,"X")
+a.update_board(3,"X")
+a.check_win
+a.draw_board
+a.update_board(5,"X")
+a.check_win
+a.draw_board
+a.update_board(7,"X")
+a.check_win
 a.draw_board
 a.reset_board
-a.draw_board
